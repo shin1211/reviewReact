e5d4028bff546097624cade6796ccbeceda5f65d
Seungmin <shin891211@gmail.com>
2022-01-05 10:31:46 -0500

    practice

---
+++ src/components/Expenses/Expense.js
@@ -0,0 +1,32 @@
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expense.css'
+function Expense({ expenses }) {
+
+    return (
+        <Card className='expenses'>
+            <ExpenseItem
+                title={expenses[0].title}
+                date={expenses[0].date}
+                amount={expenses[0].amount}
+            />
+            <ExpenseItem
+                title={expenses[1].title}
+                date={expenses[1].date}
+                amount={expenses[1].amount}
+            />
+            <ExpenseItem
+                title={expenses[2].title}
+                date={expenses[2].date}
+                amount={expenses[2].amount}
+            />
+            <ExpenseItem
+                title={expenses[3].title}
+                date={expenses[3].date}
+                amount={expenses[3].amount}
+            />
+        </Card>
+    )
+};
+
+export default Expense;
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import './ExpenseItem.css';
+import Card from '../UI/Card';
+import ExpenseDate from './ExpenseDate';
+function ExpenseItem({ date, title, amount }) {
+
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate
+                date={date}
+            />
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${amount}</div>
+            </div>
+        </Card>
+    )
+}
+
+export default ExpenseItem;
